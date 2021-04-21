9fa513f8836f0a2e48356ebd59b6ad015ac28476
mikeryan709 <mike.ryan@keyin.com>
2021-04-21 11:57:13 -0230

    profile changes

---
+++ src/pages/Profile/AccInfo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Paper,  Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+
+      '& > *': {
+        margin: theme.spacing(2),
+        width: theme.spacing(20),
+        height: theme.spacing(10),
+      },
+
+    },
+  }));
+function AccInfo() {
+    const classes = useStyles();
+    return (
+        <Paper elevation={6} className={classes.root}>
+        <Typography variant="h6">First Name: Post </Typography>
+        <Typography variant="h6">Last Name: Malone</Typography>
+        <Typography variant="h6">Email: WhiteIverson@Gmail.com</Typography>
+        <Typography variant="h6">Student ID: 1234-5678-90</Typography>
+        </Paper>
+
+    )
+}
+export default AccInfo
