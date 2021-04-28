743dbde2b6d328494dab372557711a52b03add89
ColbyO <71828390+ColbyO@users.noreply.github.com>
2021-04-28 19:06:25 -0230

    updated comments

--- src/pages/Comments/Comments.js
+++ src/pages/Comments/Comments.js
@@ -1,12 +1,92 @@
-  
-import React from 'react'
+import React from 'react';
+import './Comments.css';
+import { Container, Card, Typography, CardHeader, CardContent, Avatar } from '@material-ui/core';
+import Footer from './components/FooterBar';
+import useStyles from './styles';
+import SmsOutlinedIcon from '@material-ui/icons/SmsOutlined';
+import { useParams, Link } from 'react-router-dom';
+import comments from './data/commentData'
 
-const Comments = () => {
-    return (
-        <div>
-            <h1 className="nav-title">Comments</h1>
+function Comments() {
+  const classes = useStyles();
+  const params = useParams();
+  const id = params.id;
+
+  // let comments = JSON.parse(localStorage.getItem('comments')) || {}
+  let commentData = { id : null, username : null, title: null, body : null,  comments : []};
+  let comment_id_found = Object.keys(comments).includes(id)
+
+  if(comment_id_found){
+    commentData = comments[id];
+  }else{
+    return (<div>
+      <Typography color="error">Error, comments could not be loaded, please try again!</Typography>
+      <Link to="/"><Typography>Go back to the previous page.</Typography></Link>
+    </div>);
+  }
+
+  console.log(commentData);
+
+
+  //page layout code
+
+  console.log("REPLIES")
+  let replyHtml = [];
+  for(let i = 0; i < commentData.comments.length; i++){
+    let reply = commentData.comments[i];
+    replyHtml.push(
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="user" className={classes.avatar}>
+            {reply.username[0]}
+          </Avatar>
+        }
+        title={reply.username}
+        subheader={reply.date}
+      />
+      <CardContent>
+        <Typography color="black"
+        gutterBottom 
+        fontSize="14" 
+        align="left"
+      >{reply.message}</Typography>
+    </CardContent>
+    </Card>)
+  }
+
+  return (
+
+    //nav header, item container, comment containers  
+    <Container>
+      <div className="comment_header">
+        <h1 className="nav-title">Comment</h1>
+    </div><br/>
+
+  
+      <div className="post_container">
+        <Typography variant="head"
+        className={classes.head}>{commentData.title}</Typography>
+        <hr />
+        <div className="item_container">
+          <Typography variant="item_text"
+          gutterBottom
+          className={classes.itemText}>
+            {commentData.body}
+          </Typography>
+          
         </div>
-    )
+        <SmsOutlinedIcon className={classes.commentIcon}/>
+      </div>
+
+      <div>
+        {replyHtml}
+      </div>
+
+      <Footer />
+    </Container>
+    
+  );
 }
 
-export default Comments
+export default Comments;
--- src/pages/Messages/MessagersData.js
+++
@@ -1,8 +0,0 @@
-const the_messages = [
-    { id: 1, name: 'Steve', message: 'Will you do it for $25', image_src: './photos/Gentlemen1.jpeg'},
-    { id: 2, name: 'You', message: 'I can do it for $30', image_src: './photos/Gentlemen2.jpeg'},
-    { id: 3, name: 'Steve', message: 'Ok ill buy it for $30', image_src: './photos/Gentlemen1.jpeg'},
-    { id: 4, name: 'You', message: 'Perfect! Its yours!', image_src: './photos/Gentlemen2.jpeg'},
-]
-
-export default the_messages
